2f9a4981330d2b18f64c3444a5ac9e6aa022ba33
Lana Lisovs'ka <lana@lisovskii.com>
2024-10-23 22:24:28 +0300

    added build files

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import '@testing-library/jest-dom';
-
-test('renders Calendar component', () => {
-  render(<App />);
-  const calendar = screen.getByRole('grid');
-  expect(calendar).toBeInTheDocument();
-});
